6577a4190bb9f426608bd133058f83181a1c28f0
Kingulin <93914641+kingulin@users.noreply.github.com>
2024-02-29 09:44:47 +0100

    Add files via upload

---
+++ lista/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace lista;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ lista/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+
+namespace lista;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+	//List<string> list = new List<string>();
+	ObservableCollection<string> obCollection = new ObservableCollection<string>();
+	public MainPage()
+	{
+		InitializeComponent();
+        obCollection.Add("zjesc");
+        obCollection.Add("grac");
+        //	list.Add("spac");
+        	obCollection.Add("spac");
+        listView.ItemsSource = obCollection;
+	}
+
+	private void AddToList(object sender, EventArgs e)
+    {
+        obCollection.Add(new_entry.Text);
+    }
+
+	private void Button_Clicked(object sender, EventArgs e)
+	{
+	}
+}
+
