9f1be39a48180585deaaf2fed1a4d07a6e92a223
Kingulin <93914641+kingulin@users.noreply.github.com>
2024-02-29 09:17:23 +0100

    Add files via upload

---
+++ polsubienia/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace test;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ polsubienia/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace test;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void DeleteF(object sender, EventArgs e)
+	{
+
+		if (count == 0)
+			likeText.Text = $" {count} polubien";
+		else
+			count--;
+            likeText.Text = $" {count} polubien";
+
+	}
+    private void LIkeF(object sender, EventArgs e)
+    {
+		count++;
+        likeText.Text = $" {count} polubien";
+
+    }
+   
+}
+
